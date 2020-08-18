d67af4d0443ca66686b7ddb33013fff0eef67f16
4nndr01d <Super0rganism.com>
2020-08-18 23:22:34 +0300

    Доделана базовое редактирование заказов

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -215,17 +215,20 @@ class SiteController extends Controller
     {
         if (Yii::$app->request->post()) {
             $positions = Yii::$app->request->post();
-            $order = new Order();
-            $order->save();
-            foreach ($positions['Product'] as $product_id=>$quantity){
-                if($quantity){
-                    $position = new Position();
-                    $position->product_id = $product_id;
-                    $position->quantity = $quantity;
-                    $position->order_id = $order['id'];
-                    $position->save();
+
+            $positions = array_diff($positions['Product'], [0]);
+
+            if ($positions) {
+                $order = new Order();
+                $order->save();
+                foreach ($positions as $product_id => $quantity) {
+                        $position = new Position();
+                        $position->product_id = $product_id;
+                        $position->quantity = $quantity;
+                        $position->order_id = $order['id'];
+                        $position->save();
                 }
-            }
+        }
             return $this->redirect(['site/order-list']);
         } else {
             $product_list = Product::find()->all();
@@ -237,7 +240,6 @@ class SiteController extends Controller
 
     public function actionEditOrder(){
         if (Yii::$app->request->post()) {
-//            var_dump((Yii::$app->request->post())['Position']);
 
             $positions = (Yii::$app->request->post())['Position'];
             foreach ($positions as $position_id=>$quantity){
@@ -262,26 +264,30 @@ class SiteController extends Controller
                     $position->save();
                 }
             }
+
+            $check_positions = Position::find()->where(['order_id' => $order_id])->one();
+            if(!$check_positions){
+                $order = Order::find()->where(['id' => $order_id])->one();
+                $order->delete();
+            }
             return $this->redirect(['site/order-list']);
         } else {
             $request = Yii::$app->request;
             $id = $request->get('id');
             $order = Order::find()->where(['id' => $id])->one();
-            $positions = Position::find()->where(['order_id' => $order->id])->all();
-            $total_price = 0;
-            foreach ($positions as $position) {
-                $product = Product::find()->where(['id' => $position->product_id])->one();
-                $total_price = $total_price + ($position->quantity * $product->price);
-                $position_info = ['name'=>$product->name,'price'=>$product->price,'available_quantity'=>$product->available_quantity,'quantity'=>$position->quantity];
-                $position_list['positions'][$position->id] = $position_info;
-            }
             $products = Product::find()->all();
 
             foreach ($products as $product){
-                if(!array_key_exists( $product->id , $position_list['positions'])){
-                    $product_info = ['name'=>$product->name,'price'=>$product->price,'available_quantity'=>$product->available_quantity,'quantity'=>0];
-                    $product_list['products'][$product->id] = $product_info;
-                }
+                    $position = Position::find()->where(['product_id' => $product->id,'order_id'=>$order->id])->one();
+
+                    if($position){
+                        $position_info = ['name'=>$product->name,'price'=>$product->price,'product_id'=>$position->product_id,'available_quantity'=>$product->available_quantity,'quantity'=>$position->quantity];
+                        $position_list['positions'][$position->id] = $position_info;
+                    }else{
+                        $product_info = ['name'=>$product->name,'price'=>$product->price,'available_quantity'=>$product->available_quantity,'quantity'=>0];
+                        $product_list['products'][$product->id] = $product_info;
+                    }
+
             }
 
             return $this->render('make_order_form', ['positions' => $position_list,'products' => $product_list,'order_id'=>$order->id,'Product_model'=>new Product(),'Order'=>new Order(),'Position_model'=>new Position(), 'total_price' => $total_price, 'edit_order' => true]);
